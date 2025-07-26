4c2716a4ad0cc546ef02dc424a8ef3aeaa8d1316
VCoder <VCoder@DESKTOP-8QPRKR2>
2025-07-26 10:08:54 +0530

    add one getString method

--- src/main/java/com/common/CommonLibsApplication.java
+++ src/main/java/com/common/CommonLibsApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommonLibsApplication {
+	public String getString() {
+		return "";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommonLibsApplication.class, args);
